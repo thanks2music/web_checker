4639b3a182d8257776a65246c2ec9b0894c57f73
Yoshiyuki Ito <thanks2music@gmail.com>
2025-12-17 11:41:25 +0900

    ♻️ refactor: 非同期処理の修正と moment 依存の削除

--- functions/webCrawler.js
+++ functions/webCrawler.js
@@ -1,27 +1,40 @@
-const rp = require('request-promise');
-const moment = require('moment-timezone');
 const cheerio = require('cheerio');
 const crawler = require('./lib/crawler');
 const slackDiff = require('./lib/slack_diff');
 
-// todo: refactor
+// 日本時間でフォーマット
+const formatJST = (date) => {
+  return new Date(date).toLocaleString('ja-JP', {
+    timeZone: 'Asia/Tokyo',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+};
+
 const webCrawlerLib = async (firestore, pubsub, scheduleId, hostingUrl) => {
-  const scheduleRef = await firestore.doc(`schedules/${scheduleId}`);
+  const scheduleRef = firestore.doc(`schedules/${scheduleId}`);
   const scheduleDoc = await scheduleRef.get();
   const schedule = scheduleDoc.data();
 
   console.log('crawlerLib: ', schedule.title);
 
-  let before = +new Date;
-  let text = await crawler(encodeURI(schedule.uri), schedule.selector, true)
-      .catch( async err => {
-        console.error('crawlerLib: ', err);
-        await pubsub.topic('slackNotifier').publish(slackErrorFormat(schedule, hostingUrl, err));
-        throw err;
-      });
+  let text;
+  try {
+    text = await crawler(encodeURI(schedule.uri), schedule.selector, true);
+  } catch (err) {
+    console.error('crawlerLib: ', err);
+    await pubsub.topic('slackNotifier').publish(slackErrorFormat(schedule, hostingUrl, err));
+    throw err;
+  }
 
-  if(text === null) {
-    if(new Date().getHours() == 1) {
+  if (text === null) {
+    // 毎日1時のみ通知（通知過多を防ぐ）
+    const jstHour = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo', hour: 'numeric', hour12: false });
+    if (parseInt(jstHour) === 1) {
       console.error('crawlerLib: no content: ', schedule.title);
       await pubsub.topic('slackNotifier').publish(slackNoContentErrorFormat(schedule, hostingUrl));
     }
@@ -29,29 +42,42 @@ const webCrawlerLib = async (firestore, pubsub, scheduleId, hostingUrl) => {
   }
 
   let latestArchive;
-  const latestArchiveSnapshot = await firestore.collection(`schedules/${scheduleDoc.id}/archives`).orderBy('time', 'desc').limit(1).get();
-  latestArchiveSnapshot.forEach(archive => { latestArchive = archive.data(); });
+  const latestArchiveSnapshot = await firestore
+    .collection(`schedules/${scheduleDoc.id}/archives`)
+    .orderBy('time', 'desc')
+    .limit(1)
+    .get();
 
-  if((typeof latestArchive === 'undefined') || text != latestArchive.content) {
+  latestArchiveSnapshot.forEach(archive => {
+    latestArchive = archive.data();
+  });
+
+  if (typeof latestArchive === 'undefined' || text !== latestArchive.content) {
     const time = new Date();
 
     let latestTime = '-';
     let content = '';
-    if(latestArchive) {
-      latestTime = moment(latestArchive.time).tz('Asia/Tokyo').format();
+    if (latestArchive) {
+      latestTime = formatJST(latestArchive.time);
       content = latestArchive.content;
     }
-    await firestore.collection(`schedules/${scheduleDoc.id}/archives`).add({ content: text, time: +time });
+
+    await firestore.collection(`schedules/${scheduleDoc.id}/archives`).add({
+      content: text,
+      time: +time
+    });
 
     const newContent = cheerio
-          .load(text.replace(/<\/(.*?)>/g, '</$1>\n').replace(/<br??>/g, '<br>\n'))
-          .text()
-          .replace(/[\t| |　]+/g, ' ').replace(/\s+\n/g, '\n');
+      .load(text.replace(/<\/(.*?)>/g, '</$1>\n').replace(/<br\s*\/?>/g, '<br>\n'))
+      .text()
+      .replace(/[\t| |　]+/g, ' ')
+      .replace(/\s+\n/g, '\n');
 
     const oldContent = cheerio
-          .load(content.replace(/<\/(.*?)>/g, '</$1>\n').replace(/<br??>/g, '<br>\n'))
-          .text()
-          .replace(/[\t| |　]+/g, ' ').replace(/\s+\n/g, '\n');
+      .load(content.replace(/<\/(.*?)>/g, '</$1>\n').replace(/<br\s*\/?>/g, '<br>\n'))
+      .text()
+      .replace(/[\t| |　]+/g, ' ')
+      .replace(/\s+\n/g, '\n');
 
     const diffDisplay = slackDiff(newContent, oldContent, 'lines');
     const data = slackFormat(hostingUrl, scheduleId, schedule, time, latestTime, newContent, diffDisplay);
@@ -61,66 +87,73 @@ const webCrawlerLib = async (firestore, pubsub, scheduleId, hostingUrl) => {
     console.log('no diff: ', schedule.title);
   }
 
-  return await scheduleRef.set({ checkedAt: +new Date }, { merge: true });
+  return await scheduleRef.set({ checkedAt: +new Date() }, { merge: true });
 };
 
 module.exports = webCrawlerLib;
 
-// todo: classify arguments
 const slackFormat = (hostingUrl, scheduleId, schedule, time, latestTime, text, diff) => {
-  let titleText;
-  if(latestTime == '-') {
-    titleText = `[${schedule.title}] が新規追加されました`;
-  } else {
-    titleText = `[${schedule.title}] で更新が検知されました`;
-  }
+  const titleText = latestTime === '-'
+    ? `[${schedule.title}] が新規追加されました`
+    : `[${schedule.title}] で更新が検知されました`;
 
-  let data = {
+  const data = {
     attachments: [
       {
         title: titleText,
         title_link: schedule.uri,
         color: 'good',
         fields: [
-          { title: '更新検知日時', value: moment(time).tz('Asia/Tokyo').format(), short: true },
+          { title: '更新検知日時', value: formatJST(time), short: true },
           { title: '前回の更新日時', value: latestTime, short: true },
           { title: '差分一覧', value: `${hostingUrl}/detail.html?scheduleId=${scheduleId}` }
-
         ]
-      }, {
+      },
+      {
         fields: [
-          { title: '更新内容', value: (text.length <= 200 ? text : text.slice(0, 197) + '...'), short: false },
+          { title: '更新内容', value: text.length <= 200 ? text : text.slice(0, 197) + '...', short: false },
           { title: '差分', value: diff, short: false }
         ]
       }
     ]
   };
-  if(typeof schedule.slack !== 'undefined') { data['channel'] = schedule.slack; };
+
+  if (schedule.slack) {
+    data.channel = schedule.slack;
+  }
+
   return Buffer.from(JSON.stringify(data));
 };
 
 const slackErrorFormat = (schedule, hostingUrl, err) => {
-  let data = {
+  const data = {
     attachments: [
-      { title: `[${schedule.title}] でエラーが発生しました`,
+      {
+        title: `[${schedule.title}] でエラーが発生しました`,
         title_link: schedule.uri,
         color: 'danger',
         fields: [
           { title: 'URL', value: schedule.uri },
           { title: 'セレクタ', value: `\`${schedule.selector}\`` },
           { title: '管理ページ', value: hostingUrl },
-          { title: 'エラー内容', value: JSON.stringify(err) }
+          { title: 'エラー内容', value: err.message || JSON.stringify(err) }
         ]
-      }]
+      }
+    ]
   };
-  if(typeof schedule.slack !== 'undefined') { data['channel'] = schedule.slack; };
+
+  if (schedule.slack) {
+    data.channel = schedule.slack;
+  }
+
   return Buffer.from(JSON.stringify(data));
 };
 
 const slackNoContentErrorFormat = (schedule, hostingUrl) => {
-  let data = {
+  const data = {
     attachments: [
-      { title: `[${schedule.title}] でコンテンツが存在しませんでした`,
+      {
+        title: `[${schedule.title}] でコンテンツが存在しませんでした`,
         title_link: schedule.uri,
         color: 'warning',
         fields: [
@@ -128,8 +161,13 @@ const slackNoContentErrorFormat = (schedule, hostingUrl) => {
           { title: 'セレクタ', value: `\`${schedule.selector}\`` },
           { title: '管理ページ', value: hostingUrl }
         ]
-      }]
+      }
+    ]
   };
-  if(typeof schedule.slack !== 'undefined') { data['channel'] = schedule.slack; };
+
+  if (schedule.slack) {
+    data.channel = schedule.slack;
+  }
+
   return Buffer.from(JSON.stringify(data));
 };
--- functions/webFetcher.js
+++ functions/webFetcher.js
@@ -1,26 +1,29 @@
-const rp = require('request-promise');
 const cronParser = require('cron-parser');
-const moment = require('moment-timezone');
 
 const webFetcher = async (firestore, pubsub) => {
   const schedules = await firestore.collection('schedules').get();
 
-  return await schedules.forEach(async schedule => {
-    if(isAlreadyChecked(schedule.data())) { return; }
+  const publishPromises = schedules.docs
+    .filter(schedule => !isAlreadyChecked(schedule.data()))
+    .map(async schedule => {
+      console.log('publish webChecker', schedule.data().title);
+      const data = JSON.stringify({ scheduleId: schedule.id });
+      const dataBuffer = Buffer.from(data);
+      await pubsub.topic('webChecker').publish(dataBuffer);
+    });
 
-    console.log('publish webChecker', schedule.data().title);
-    const data = JSON.stringify({ scheduleId: schedule.id });
-    const dataBuffer = Buffer.from(data);
-    await pubsub.topic('webChecker').publish(dataBuffer);
-  });
+  await Promise.all(publishPromises);
 };
 
 module.exports = webFetcher;
 
 const isAlreadyChecked = (schedule) => {
-  if(typeof schedule.checkedAt === 'undefined') { return false; }
-  let crontab = schedule.schedule;
-  const prev = cronParser.parseExpression('* * * * *', { tz: 'Asia/Tokyo'}).prev().toDate();
-  const checked = moment(schedule.checkedAt).toDate();
-  return (prev < checked);
+  if (typeof schedule.checkedAt === 'undefined') { return false; }
+
+  const prev = cronParser.parseExpression('* * * * *', {
+    tz: 'Asia/Tokyo'
+  }).prev().toDate();
+
+  const checked = new Date(schedule.checkedAt);
+  return prev < checked;
 };
